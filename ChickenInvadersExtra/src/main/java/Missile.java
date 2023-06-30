11c9215fa9f94df48da81311a25b506a529b0380
pashaz7b <ap550697@gmail.com>
2023-06-30 14:32:42 +0330

    Missile class updated

--- ChickenInvadersExtra/src/main/java/Missile.java
+++ ChickenInvadersExtra/src/main/java/Missile.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import processing.core.*;
+import processing.sound.SoundFile;
 
 public class Missile implements MissileInterface {
     private float x;
@@ -7,24 +9,39 @@ public class Missile implements MissileInterface {
     private int missileColor;
     private float missileWidth;
     private float missileHeight;
+    private PImage missileImage;
+    private SoundFile missileSound;
+    private boolean soundPlayed;
 
     public Missile(float x, float y) {
         this.x = x;
         this.y = y;
         this.speed = -5;
         this.missileColor = Main.pApplet.color(255, 255, 255);
-        this.missileWidth = 5;
-        this.missileHeight = 10;
+        this.missileWidth = 15;
+        this.missileHeight = 30;
+
+        // Load missile image and sound
+        this.missileImage = Main.pApplet.loadImage("missile.png");
+        this.missileSound = new SoundFile(Main.pApplet, "missile_sound.wav");
+        this.soundPlayed = false; // Initialize soundPlayed as false
     }
 
     public void update() {
         y += speed;
+        // Play missile sound if it has just been fired and the sound has not been played yet
+        if (y + speed <= Main.pApplet.height && !soundPlayed) {
+            missileSound.play();
+            soundPlayed = true; // Set soundPlayed to true after playing the sound
+        }
     }
 
     public void drawMissile() {
         Main.pApplet.fill(missileColor);
-        Main.pApplet.rect(x, y, missileWidth, missileHeight);
+        // Draw missile using the image
+        Main.pApplet.image(missileImage, x, y, missileWidth, missileHeight);
     }
+
 
     public float getX() {
         return x;
